fbe08ef1b72f3a7ab428c99da4fe252b2aa19eb7
Developer <santhosh.hegde262000@gmail.com>
2025-03-07 15:47:14 +0530

    pagination done

--- routes/user.js
+++ routes/user.js
@@ -47,12 +47,16 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    if (limit > 10) {
+      limit = 10;
+    }
+    const skip = (page - 1) * limit;
     const loggedInUser = req.user;
     const connectionRequest = await ConnectionRequestModel.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select("fromUserId toUserId");
-    // .populate("fromUserId", "firstName")
-    // .populate("toUserId", "firstName");
     const userExclude = new Set();
     connectionRequest.forEach((user) => {
       if (user.fromUserId.equals(loggedInUser._id)) {
@@ -66,7 +70,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(userExclude) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
     res.json({ message: "User list", data: feedUser });
   } catch (err) {
     res.status(400).json("Error: " + err.message);
